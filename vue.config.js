fef12be4a1de66f76dd180af8259b38063e49bf1
markd964 <devonmark79@gmail.com>
2026-03-19 15:59:19 +0800

    fix: use npx for vue-cli-service, disable lint on build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'dist',
   publicPath: '/',
+  lintOnSave: false,
 })
